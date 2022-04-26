56312122749e989cbed0940548a20f902e1f3053
Lucas Matheus Westphal <lmwestphal@furb.br>
2022-04-25 21:52:53 -0300

    Aula java

---
+++ aula1/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    //comentário de linha
+    /* comentário de bloco
+    block
+    block
+    block
+     */
+    static int var1 = 10;
+    double var2 = 25.8;
+    char var3 = 'a';
+    String var4 = "abcdefghij";
+    boolean var5 = true;
+
+    public static void main(String[] args) {
+        int var1 = 10;
+        String var2 = "10";
+        System.out.println(var1 + var2);
+    }
+    //private
+    //protected
+
+}
