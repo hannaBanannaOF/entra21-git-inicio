8c0e7d86ec7d36e65851d044f477b9e92f3fa3c6
Lucas Matheus Westphal <lmwestphal@furb.br>
2022-08-29 21:52:14 -0300

    spring

---
+++ primeiro-projeto-spring/src/main/java/com/entra21/primeiroprojetospring/PrimeiroProjetoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.entra21.primeiroprojetospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroProjetoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroProjetoSpringApplication.class, args);
+	}
+
+}
---
+++ primeiro-projeto-spring/src/test/java/com/entra21/primeiroprojetospring/PrimeiroProjetoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entra21.primeiroprojetospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroProjetoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
