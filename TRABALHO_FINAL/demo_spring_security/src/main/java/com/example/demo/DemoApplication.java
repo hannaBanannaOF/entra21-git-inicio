6c99015a894269e34945c484b5894dd77245c324
Lucas Matheus Westphal <lmwestphal@furb.br>
2022-09-08 21:41:32 -0300

    spring sec

--- TRABALHO_FINAL/demo_spring_security/src/main/java/com/example/demo/DemoApplication.java
+++ TRABALHO_FINAL/demo_spring_security/src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,26 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Configuration
+	public static class SecurityConfig {
+
+		@Bean
+		public static PasswordEncoder passwordEncoder() {
+			return NoOpPasswordEncoder.getInstance();
+		}
 	}
 
 }
--- TRABALHO_FINAL/demo_spring_security/src/main/java/com/example/demo/MyUserPrincipal.java
+++ TRABALHO_FINAL/demo_spring_security/src/main/java/com/example/demo/MyUserPrincipal.java
@@ -29,17 +29,17 @@ public class MyUserPrincipal implements UserDetails {
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
